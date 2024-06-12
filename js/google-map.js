983ff1b59384a446b42629c7d58c4d1a4d7fc798
Isaac Carvalho <carvalhoisaac.eng+github@gmail.com>
2024-06-12 19:06:24 -0300

    algumas alterações de encaixe de texto entre paginas

--- js/google-map.js
+++ js/google-map.js
@@ -1,11 +1,10 @@
-
 var google;
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(-9.747402241339776, -36.67195498763871);
     // 39.399872
     // -8.224454
     
